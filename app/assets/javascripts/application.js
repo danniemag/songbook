12a2c57b2acec8286606f4d2cd95bdb67339ad80
Dannie Magalhães <seliksy@gmail.com>
2018-09-01 22:13:36 -0300

    Login/Logout issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -11,9 +12,9 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
+//= require activestorage
 //= require ckeditor/init
 //= require_tree .
-
-//= require jquery
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
     root to: 'dashboard#home', as: :authenticated_root
   end
 
-  root to: 'dashboard#home'
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
 
   devise_for :users, controllers: {
       omniauth_callbacks: 'users/omniauth_callbacks',
